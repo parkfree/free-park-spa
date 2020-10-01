c6f882e473140ce5ca71c82655ece69f7714b0b8
Chen Liang <slimhigh@gmail.com>
2020-10-01 08:44:11 +0800

    Add a navbar.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,36 @@
 <template>
-  <div class="tile">
-    {{user.owner}}
+  <div class="columns">
+    <div class="column is-three-fifths is-offset-one-fifth">
+      <b-navbar>
+        <template slot="brand">
+          <b-navbar-item tag="router-link" :to="{ path: '/' }">
+            <b-icon
+                icon="parking"
+                size="is-large"
+                type="is-primary">
+            </b-icon>
+            <div class="has-text-weight-bold is-size-5">Free Park</div>
+          </b-navbar-item>
+        </template>
+
+        <template slot="start">
+        </template>
+
+        <template slot="end">
+          <b-navbar-item href="#">
+            管理界面
+          </b-navbar-item>
+          <b-navbar-dropdown :label="user.owner">
+            <b-navbar-item href="#">
+              个人设置
+            </b-navbar-item>
+            <b-navbar-item href="#">
+              退出
+            </b-navbar-item>
+          </b-navbar-dropdown>
+        </template>
+      </b-navbar>
+    </div>
   </div>
 </template>
 
