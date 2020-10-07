c85943f05625fab91c80c9f5d22375a666ef94ef
Chen Liang <slimhigh@gmail.com>
2020-10-07 14:16:48 +0800

    Create separate UserContainer.

---
+++ src/components/UserContainer.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="has-background-light">
+    <div class="columns">
+      <div class="column is-three-fifths is-offset-one-fifth">
+        <div class="block">
+          <b-navbar shadow>
+            <template slot="brand">
+              <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <b-icon
+                    icon="parking"
+                    size="is-large"
+                    type="is-primary">
+                </b-icon>
+                <div class="has-text-weight-bold is-size-5 has-text-primary">Free Park</div>
+              </b-navbar-item>
+            </template>
+
+            <template slot="start">
+            </template>
+
+            <template slot="end">
+              <b-navbar-dropdown :label="user.owner">
+                <b-navbar-item href="#">
+                  个人设置
+                </b-navbar-item>
+                <b-navbar-item @click="logout">
+                  退出
+                </b-navbar-item>
+              </b-navbar-dropdown>
+            </template>
+          </b-navbar>
+        </div>
+        <slot></slot>
+      </div>
+    </div>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Free Park</strong> is built with <a href="https://buefy.org/" target="_blank">Buefy</a>, <a
+            href="https://bulma.io" target="_blank">Bulma</a>, and <a href="https://vuejs.org"
+                                                                      target="_blank">Vue.js</a>.
+        </p>
+      </div>
+    </footer>
+  </section>
+
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'UserContainer',
+  methods: {
+    logout () {
+      this.$store.dispatch('logout')
+        .then(() => this.$router.push('/login'))
+    }
+  },
+  mounted () {
+    this.$store.dispatch('getUser')
+  },
+  computed: {
+    ...mapState(['user'])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,76 +1,24 @@
 <template>
-  <section class="has-background-light">
-    <div class="columns">
-      <div class="column is-three-fifths is-offset-one-fifth">
-        <div class="block">
-          <b-navbar shadow>
-            <template slot="brand">
-              <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <b-icon
-                    icon="parking"
-                    size="is-large"
-                    type="is-primary">
-                </b-icon>
-                <div class="has-text-weight-bold is-size-5 has-text-primary">Free Park</div>
-              </b-navbar-item>
-            </template>
-
-            <template slot="start">
-            </template>
-
-            <template slot="end">
-              <b-navbar-dropdown :label="user.owner">
-                <b-navbar-item href="#">
-                  个人设置
-                </b-navbar-item>
-                <b-navbar-item @click="logout">
-                  退出
-                </b-navbar-item>
-              </b-navbar-dropdown>
-            </template>
-          </b-navbar>
-        </div>
-        <car-status></car-status>
-        <payment-list></payment-list>
-        <member-list></member-list>
-      </div>
-    </div>
-    <footer class="footer">
-      <div class="content has-text-centered">
-        <p>
-          <strong>Free Park</strong> is built with <a href="https://buefy.org/" target="_blank">Buefy</a>, <a
-            href="https://bulma.io" target="_blank">Bulma</a>,
-          and <a href="https://vuejs.org" target="_blank">Vue.js</a>.
-        </p>
-      </div>
-    </footer>
-  </section>
+  <user-container>
+    <car-status></car-status>
+    <payment-list></payment-list>
+    <member-list></member-list>
+  </user-container>
 </template>
 
 <script>
-import { mapState } from 'vuex'
 import CarStatus from '@/components/CarStatus'
 import PaymentList from '@/components/TodayPayment'
 import MemberList from '@/components/MemberList'
+import UserContainer from '@/components/UserContainer'
 
 export default {
   name: 'Home',
   components: {
     CarStatus,
     PaymentList,
-    MemberList
+    MemberList,
+    UserContainer
   },
-  methods: {
-    logout () {
-      this.$store.dispatch('logout')
-        .then(() => this.$router.push('/login'))
-    }
-  },
-  mounted () {
-    this.$store.dispatch('getUser')
-  },
-  computed: {
-    ...mapState(['user'])
-  }
 }
 </script>
