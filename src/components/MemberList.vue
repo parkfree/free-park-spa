be3fd47114141f5c9a0c2aa7612454a41d17bf98
Chen Liang <slimhigh@gmail.com>
2020-10-08 12:15:01 +0800

    Display name enablePay and enablePoint in the member list.

--- src/components/MemberList.vue
+++ src/components/MemberList.vue
@@ -17,22 +17,40 @@
           <p class="has-text-centered">您还没有添加账号哦~</p>
         </template>
         <template v-else>
-          <b-table :data="members" striped hoverable>
+          <b-table :data="members" striped hoverable detailed>
             <b-table-column field="mobile" label="手机号" v-slot="props">
               {{ props.row.mobile }}
             </b-table-column>
+
+            <b-table-column field="name" label="名字" v-slot="props">
+              {{ props.row.name }}
+            </b-table-column>
+
+            <b-table-column field="enablePoint" label="参与领积分" v-slot="props">
+              <b-tag v-if="props.row.enablePoint" type="is-success">是</b-tag>
+              <b-tag v-else>否</b-tag>
+            </b-table-column>
+
+            <b-table-column field="enablePay" label="参与缴费" v-slot="props">
+              <b-tag v-if="props.row.enablePay" type="is-success">是</b-tag>
+              <b-tag v-else>否</b-tag>
+            </b-table-column>
+
             <b-table-column field="lastPaidAt" label="缴费日期" v-slot="props">
               {{ props.row.lastPaidAt }}
             </b-table-column>
-            <b-table-column field="userId" label="userId" v-slot="props">
-              {{ props.row.userId }}
-            </b-table-column>
+
             <b-table-column v-slot="props">
               <div class="buttons">
                 <b-button type="is-primary" size="is-small" icon-right="user-edit" @click="updateMember(props.row.id)"/>
                 <b-button type="is-danger" size="is-small" icon-right="trash-alt" @click="confirmDelete(props.row.id)"/>
               </div>
             </b-table-column>
+
+            <template slot="detail" slot-scope="props">
+              <p><strong>userId:</strong> <code>{{props.row.userId}}</code></p>
+              <p><strong>openId:</strong> <code>{{props.row.openId}}</code></p>
+            </template>
           </b-table>
         </template>
       </div>
