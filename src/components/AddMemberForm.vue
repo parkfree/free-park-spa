5824728eb6c38faca0d622f2c43bad0183747ffe
Chen Liang <slimhigh@gmail.com>
2020-10-08 14:37:55 +0800

    Add member name enablePay and enablePoint in update and create form.

--- src/components/AddMemberForm.vue
+++ src/components/AddMemberForm.vue
@@ -10,6 +10,9 @@
           <b-input required v-model="member.mobile" placeholder="手机号" type="tel" pattern="[0-9]{11}"
                    validation-message="输入11位手机号"></b-input>
         </b-field>
+        <b-field label="名字">
+          <b-input required v-model="member.name" placeholder="名字" type="text"></b-input>
+        </b-field>
         <b-field label="userId">
           <b-input required v-model="member.userId" placeholder="userId" type="text" pattern="[a-f0-9]{32}"
                    validation-message="只允许字符：[a-f0-9]，且长度必须为32"></b-input>
@@ -18,6 +21,16 @@
         <b-field label="openId">
           <b-input required v-model="member.openId" placeholder="openId" type="text" pattern="[A-Za-z0-9_\-]{28}"
                    validation-message="只允许字符：[A-Za-z0-9_\-]，且长度必须为28"></b-input>
+        </b-field>
+
+        <b-field grouped>
+          <b-field label="是否参与领积分">
+            <b-switch v-model="member.enablePoint">{{member.enablePoint ? '是' : '否'}}</b-switch>
+          </b-field>
+
+          <b-field label="是否参与缴费">
+            <b-switch v-model="member.enablePay">{{member.enablePay ? '是' : '否'}}</b-switch>
+          </b-field>
         </b-field>
 
         <b-field label="会员类型">
@@ -39,7 +52,10 @@ export default {
         mobile: '',
         userId: '',
         openId: '',
-        memType: 'in卡'
+        memType: 'in卡',
+        name: '',
+        enablePay: true,
+        enablePoint: false
       }
     }
   },
