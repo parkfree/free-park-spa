34e940202b34f71e38a35e077bc18f94f0673844
Chen Liang <slimhigh@gmail.com>
2020-10-14 22:12:33 +0800

    Change car status component to use box instead of card.

--- src/components/CarStatus.vue
+++ src/components/CarStatus.vue
@@ -1,49 +1,45 @@
 <template>
-  <div class="block">
-    <div class="card">
-      <div class="card-content">
-        <div class="block">
-          <div class="level">
-            <div class="level-left">
-              <div class="level-item">
-                <span class="has-text-weight-bold mr-2">{{user.carNumber}}</span>
-                <b-tag :type="status !== 'none' ? 'is-success' : ''">{{statusTag}}</b-tag>
-              </div>
-            </div>
-            <div class="level-right">
-              <div v-if="user.totalPaidAmount" class="level-item">
-                已累计为您节省：<strong class="is-size-5 has-text-primary">{{user.totalPaidAmount | currency}}</strong>
-              </div>
-            </div>
-          </div>
-          <div>
+  <div class="box">
+    <div class="block">
+      <div class="level">
+        <div class="level-left">
+          <div class="level-item">
+            <span class="has-text-weight-bold mr-2">{{user.carNumber}}</span>
+            <b-tag :type="status !== 'none' ? 'is-success' : ''">{{statusTag}}</b-tag>
           </div>
         </div>
-        <template v-if="status === 'paying'">
-          <div class="block">
-            <p>您的车已于 {{task.parkAt | onlyTime}} 进入停车场，缴费任务于 {{task.createdAt | onlyTime }} 启动，第一次缴费时间为
-              {{firstScheduleDateTime}}，每隔 {{task.periodMinutes}} 分钟缴费一次，下次缴费时间为 {{task.nextScheduledAt | onlyTime}}。</p>
+        <div class="level-right">
+          <div v-if="user.totalPaidAmount" class="level-item">
+            已累计为您节省：<strong class="is-size-5 has-text-primary">{{user.totalPaidAmount | currency}}</strong>
           </div>
-          <b-button type="is-danger" @click="cancelPayTask">取消自动缴费</b-button>
-        </template>
-
-        <template v-if="status === 'checking'">
-          <div class="block">
-            <p>正在检测您的车是否进入停车场。检测任务于 {{task.createdAt | onlyTime }} 启动，第一次检测时间为 {{firstScheduleDateTime}}，每隔
-              {{task.periodMinutes}} 分钟检测一次，已检测了 {{task.checkCount}} 次，下次检测时间为
-              {{task.nextScheduledAt | onlyTime}}。{{task.checkCountLimit}} 次检测不到之后取消。</p>
-          </div>
-          <b-button type="is-danger" @click="cancelCheckTask">取消检测任务</b-button>
-        </template>
-
-        <template v-if="status === 'none'">
-          <div class="block">
-            <p>周一到周五每天早上 08:30 启动自动缴费任务。您也可以点击下方按钮手动触发今天的自动缴费。</p>
-          </div>
-          <b-button @click="createCheckTask">触发自动缴费</b-button>
-        </template>
+        </div>
+      </div>
+      <div>
       </div>
     </div>
+    <template v-if="status === 'paying'">
+      <div class="block">
+        <p>您的车已于 {{task.parkAt | onlyTime}} 进入停车场，缴费任务于 {{task.createdAt | onlyTime }} 启动，第一次缴费时间为
+          {{firstScheduleDateTime}}，每隔 {{task.periodMinutes}} 分钟缴费一次，下次缴费时间为 {{task.nextScheduledAt | onlyTime}}。</p>
+      </div>
+      <b-button type="is-danger" @click="cancelPayTask">取消自动缴费</b-button>
+    </template>
+
+    <template v-if="status === 'checking'">
+      <div class="block">
+        <p>正在检测您的车是否进入停车场。检测任务于 {{task.createdAt | onlyTime }} 启动，第一次检测时间为 {{firstScheduleDateTime}}，每隔
+          {{task.periodMinutes}} 分钟检测一次，已检测了 {{task.checkCount}} 次，下次检测时间为
+          {{task.nextScheduledAt | onlyTime}}。{{task.checkCountLimit}} 次检测不到之后取消。</p>
+      </div>
+      <b-button type="is-danger" @click="cancelCheckTask">取消检测任务</b-button>
+    </template>
+
+    <template v-if="status === 'none'">
+      <div class="block">
+        <p>周一到周五每天早上 08:30 启动自动缴费任务。您也可以点击下方按钮手动触发今天的自动缴费。</p>
+      </div>
+      <b-button @click="createCheckTask">触发自动缴费</b-button>
+    </template>
   </div>
 </template>
 
