36b801635789d1be0507195195e21e1dac0d7d3c
Chen Liang <slimhigh@gmail.com>
2020-10-15 21:22:41 +0800

    Set router link and active class to admin menu items.

--- src/views/admin/Admin.vue
+++ src/views/admin/Admin.vue
@@ -9,12 +9,15 @@
         </div>
         <b-menu class="is-custom-mobile">
           <b-menu-list label="menu">
-            <b-menu-item icon="tachometer-alt" label="控制台" @click="$router.push('/admin')"></b-menu-item>
-            <b-menu-item icon="users" label="账号列表" @click="$router.push('/admin/members')"></b-menu-item>
-            <b-menu-item icon="tags" label="缴费记录"></b-menu-item>
+            <b-menu-item icon="tachometer-alt" label="控制台"
+                         tag="router-link" to="/admin" active-class="is-active" exact></b-menu-item>
+            <b-menu-item icon="users" label="账号列表"
+                         tag="router-link" to="/admin/members" active-class="is-active" exact></b-menu-item>
+            <b-menu-item icon="tags" label="缴费记录"
+                         tag="router-link" to="/admin/payments" active-class="is-active" exact></b-menu-item>
           </b-menu-list>
           <b-menu-list label="自己">
-            <b-menu-item icon="user" label="个人主页"></b-menu-item>
+            <b-menu-item icon="user" label="个人主页" tag="router-link" to="/"></b-menu-item>
             <b-menu-item icon="sign-out-alt" label="退出"></b-menu-item>
           </b-menu-list>
         </b-menu>
@@ -28,7 +31,7 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       expandOnHover: false,
       mobile: 'reduce',
@@ -39,93 +42,93 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.p-1 {
-  padding: 1em;
-}
-
-.sidebar-layout {
-  display: flex;
-  flex-direction: row;
-  align-items: stretch;
-
-  .sidebar {
+  .p-1 {
     padding: 1em;
-    min-height: 100vh;
   }
 
-  .main-content {
-    padding: 1em;
-    width: 100%;
-  }
-}
+  .sidebar-layout {
+    display: flex;
+    flex-direction: row;
+    align-items: stretch;
 
-@media screen and (max-width: 1023px) {
-  .b-sidebar {
-    .sidebar-content {
-      &.is-mini-mobile {
-        &:not(.is-mini-expand),
-        &.is-mini-expand:not(:hover) {
-          .menu-list {
-            li {
-              a {
-                span:nth-child(2) {
-                  display: none;
+    .sidebar {
+      padding: 1em;
+      min-height: 100vh;
+    }
+
+    .main-content {
+      padding: 1em;
+      width: 100%;
+    }
+  }
+
+  @media screen and (max-width: 1023px) {
+    .b-sidebar {
+      .sidebar-content {
+        &.is-mini-mobile {
+          &:not(.is-mini-expand),
+          &.is-mini-expand:not(:hover) {
+            .menu-list {
+              li {
+                a {
+                  span:nth-child(2) {
+                    display: none;
+                  }
                 }
-              }
 
-              ul {
-                padding-left: 0;
+                ul {
+                  padding-left: 0;
 
-                li {
-                  a {
-                    display: inline-block;
+                  li {
+                    a {
+                      display: inline-block;
+                    }
                   }
                 }
               }
             }
-          }
 
-          .menu-label:not(:last-child) {
-            margin-bottom: 0;
+            .menu-label:not(:last-child) {
+              margin-bottom: 0;
+            }
           }
         }
       }
     }
   }
-}
 
-@media screen and (min-width: 1024px) {
-  .b-sidebar {
-    .sidebar-content {
-      &.is-mini {
-        &:not(.is-mini-expand),
-        &.is-mini-expand:not(:hover) {
-          .menu-list {
-            li {
-              a {
-                span:nth-child(2) {
-                  display: none;
+  @media screen and (min-width: 1024px) {
+    .b-sidebar {
+      .sidebar-content {
+        &.is-mini {
+          &:not(.is-mini-expand),
+          &.is-mini-expand:not(:hover) {
+            .menu-list {
+              li {
+                a {
+                  span:nth-child(2) {
+                    display: none;
+                  }
                 }
-              }
 
-              ul {
-                padding-left: 0;
+                ul {
+                  padding-left: 0;
 
-                li {
-                  a {
-                    display: inline-block;
+                  li {
+                    a {
+                      display: inline-block;
+                    }
                   }
                 }
               }
             }
-          }
 
-          .menu-label:not(:last-child) {
-            margin-bottom: 0;
+            .menu-label:not(:last-child) {
+              margin-bottom: 0;
+            }
           }
         }
       }
     }
   }
-}
 </style>
